50b904e194458c24953ff00eb4251ba0adc37fc3
Ubuntu <terraform@tf-poc-01.bcjkobsdd2du3inuyfvawp0yga.rx.internal.cloudapp.net>
2024-11-15 08:47:44 +0000

    Added azurerm

--- terraformcode/azvm/main.tf
+++ terraformcode/azvm/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"  // Replace with your desired version constraint
+    }
+  }
+}
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
